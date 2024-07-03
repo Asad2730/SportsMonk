c9c506953477ce6ffcd5e2b3c316e1e3906df5b3
Asad2730 <asadsajjad777@gmail.com>
2024-07-04 04:02:34 +0500

    ...

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,32 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get(':url')
-  async fetchAndSave(@Param('url') url: string) {
+  @Get('fetch')
+  async fetchAndSave(@Query('url') url: string, @Query('apiToken') apiToken: string) {
+    const apiUrl = `${url}?api_token=${apiToken}`;
     try {
-      const data = await this.appService.fetchDataFromApi(url);
-      const tableName = data.resource;
+      const data = await this.appService.fetchDataFromApi(apiUrl);
+
+      if (!data.data || !Array.isArray(data.data) || data.data.length === 0) {
+        throw new Error('Invalid data format or empty data');
+      }
+
+      const tableName = data.data[0].resource; 
+
+      if (!tableName) {
+        throw new Error('Resource name not found in the data');
+      }
+
       await this.appService.ensureTableExists(tableName);
       await this.appService.saveOrUpdateData(tableName, data.data);
+
       return 'Data fetched and saved successfully!';
     } catch (error) {
-      return `Error: ${error.message}`;
+      return `Error: ${error.message} url:${apiUrl}`;
     }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,16 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HttpModule } from '@nestjs/axios';
 
+
 @Module({
   imports: [
      TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: 'your_db_username',
-      password: 'your_db_password',
-      database: 'your_db_name',
+      username: 'postgres',
+      password: '123',
+      database: 'CricketDms',
       synchronize: true,
       entities: [],
      }),

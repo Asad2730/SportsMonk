67a5a2c9bf3086a668bce3bd9c6b47b0a3a6b1cf
Asad2730 <asadsajjad777@gmail.com>
2024-07-16 01:01:08 +0500

    ...

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
+  app.enableCors({
+    origin: 'http://localhost:3001', // Adjust this to match your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
